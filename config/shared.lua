cd654f9efeeac701b8c8aea04a9f8423fa8615b1
Magnitrude <100609084+Magnitrude@users.noreply.github.com>
2024-09-08 17:06:25 -0400

    Update shared.lua

--- config/shared.lua
+++ config/shared.lua
@@ -27,6 +27,12 @@ return {
             backgroundImage = 'https://i.ibb.co/vxvGzg1/card.png',
             prop = 'prop_franklin_dl',
         },
+        ['huntinglicense'] = {
+            header = 'Hunting License',
+            background = '#013613',
+            backgroundImage = 'https://i.ibb.co/vxvGzg1/card.png',
+            prop = 'prop_franklin_dl',
+        },
         ['lawyerpass'] = {
             header = 'Lawyer Pass',
             background = '#f9c491',
